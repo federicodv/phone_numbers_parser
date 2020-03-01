ee701af0b5567c0ab016158a11066017177c40e8
Federico del Valle <quaidy@gmail.com>
2020-02-29 21:03:17 -0300

    First commit: folders parser

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # phone_numbers_parser
+
+## Task:
+
+    Write an application that finds all phone numbers in a set of text files located in a directory tree starting with <somewhere>. 
+    Files should be processed regardless of the nesting level. 
+    At the same time, only text files with .txt extension need to be processed, and the others should be ignored.
+    
+    Phone numbers in the source files can be given with the country code:
+    +7 812 number
+    +7 (495) number
+    +7812number
+    +7812 number
+    7-812-number
+    
+    With a three-digit area code:
+    (812) number
+    812number
+    812 number
+    095-number
+
+    Or none at all. At the same time the number can have different spellings:
+    123-4567
+    123-45-67
+    1234567
+    
+    If the city code is not specified, it is considered equal to 812, 
+    if the country code is not specified, it is considered equal to 7.
+    You need to find all the numbers in all of the files. 
+    
+    Change formatting to the unified "full" format
+    +7 (812) 123-4567
+    
+    remove duplicates and print the list of numbers in ascending order.
+
+## Asumptions  
+1. National numbers always have 7 digits
+2. Only numeric phones are valid  
+    i.e:   
+    +7 (812)-1CALLME isn't valid
+    +7 (812)-1234567 is valid
+3. Area codes have 3 digits
+4. Country code have 1 digit
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+def parse_number(phone_number, country=7, area=812):
+    phone_number = "".join(re.findall('[0-9]', phone_number))
+    if len(phone_number) <= 11 and len(phone_number) > 6: 
+        if len(phone_number) == 11 or len(phone_number) == 8:
+            country, phone_number = phone_number[0], phone_number[1::]
+        if len(phone_number) == 10:
+            area, phone_number = phone_number[0:3], phone_number[3::]
+        return "{0}{1}{2}".format(country, area, phone_number)
+    return ""    
+
+def build_file_path(base_path, file_data):
+    if file_data[-4::] == ".txt":
+        return "{0}/{1}".format(base_path, file_data)
+    return ""
+
+def format_numbers(phone_numbers):
+    return list(map(lambda phone: "+{0} ({1}) {2}-{3}".format(phone[0], phone[1:4], phone[4:7], phone[7::]), phone_numbers))
+
+def extract_phonenumbers(path="./phones"):
+    paths = list(os.walk(path))
+    numbers = []
+    for path_data in paths:
+        if path_data[2] != []:
+            files_paths = [build_file_path(path_data[0], file_data) for file_data in path_data[2]]
+            for numbers_file_path in list(files_paths):
+                if numbers_file_path != "":
+                    with open(numbers_file_path) as f:
+                        numbers.extend([parse_number(line.rstrip('\n')) for line in f])
+    normalized_numbers = list(set(numbers))
+    if normalized_numbers[0] == "":
+        normalized_numbers.pop(0)
+    normalized_numbers.sort()
+    return format_numbers(normalized_numbers)
+
+print('Formatting numbers ...')
+print(extract_phonenumbers())
